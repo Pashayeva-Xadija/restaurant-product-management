2a06be6ea510582213bd086161e0c58b668ddb8b
Pashayeva-Xadija <xadijapashayeva@gmail.com>
2025-08-28 12:16:19 +0400

    Initial commit

--- src/main/java/com/restaurant/productmanagement/dto/RegisterDto.java
+++ src/main/java/com/restaurant/productmanagement/dto/RegisterDto.java
@@ -1,9 +1,18 @@
 package com.restaurant.productmanagement.dto;
 
+
+
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
+
 @Data
 public class RegisterDto {
+    @JsonAlias("a")
     private String name;
+
+    @JsonAlias("b")
     private String email;
+
+    @JsonAlias("c")
     private String password;
 }
